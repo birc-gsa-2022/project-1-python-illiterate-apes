020c11a42d25b98db71caeb8e1a03aa50763c614
Atamarado <52821119+Atamarado@users.noreply.github.com>
2022-09-12 13:41:26 +0200

    Different generators created and documented

--- src/generators.py
+++ src/generators.py
@@ -1,19 +1,82 @@
 import random
 import string
 
-def generate_random_sequence(minLength, maxLength, alphabet):
-    chainLength = random.choice(range(minLength, maxLength+1))
+def generate_random_sequence(chainLength, alphabet):
+    """
+    Returns a string of length='chainLength' made up from random characters from 'alpabet'
+    Arguments:
+    chainLength: Expects an integer > 0 to determine the length of the string output
+    alphabet: A Python list containing all valid characters
+    """
     # If you try to join a non empty string with this it explodes
     output = "".join(random.choice(alphabet) for _ in range(chainLength))
     return output
 
-# def generate_random
+def generate_same_before(chainLength, alphabet, prob):
+    """
+    Returns a string of length='chainLength' made up from random characters from 'alphabet'
+    For every character a random float between 0 and 1 is chosen: if it's lower than prob
+    the character used for generating the previous character is used
+    chainLength: Expects an integer > 0 to determine the length of the string output
+    alphabet: A Python list containing all valid characters
+    prob: A probability between 0 and 1
+    """
+    output = ""
+    selectedChar = random.choice(alphabet)
+    for i in range(chainLength):
+        if random.random()>=prob:
+            selectedChar = random.choice(alphabet)
+        output += selectedChar
+    return output
+
+def generate_multiple(chainLength, alphabet, prob, minStrike, maxStrike):
+    """
+    Returns a string of length='chainLength' made up from random characters from 'alphabet'
+    For every character a random float between 0 and 1 is chosen: if it's lower than prob
+    the character is printed n times, where n is a random value between 'minStrike' and 'maxStrike'
+    (included)
+    chainLength: Expects an integer > 0 to determine the length of the string output
+    alphabet: A Python list containing all valid characters
+    prob: A probability between 0 and 1
+    minStrike: The minimum amount of characters that will appear in a strike
+    maxStrike: The maximum amount of characters that will appear in a strike
+    """
+    output = ""
+    for i in range(chainLength):
+        selectedChar = random.choice(alphabet)
+        if random.random()>=prob:
+            output += selectedChar
+        else:
+            strikeLength = random.choice(range(minStrike, maxStrike+1))
+            if strikeLength > chainLength-i:
+                strikeLength = i
+            output += "".join(selectedChar for _ in range(strikeLength))
+            i += strikeLength-1
+    return output
+
+def generate_different(chainLength, alphabet):
+    """
+    Returns a string of length='chainLength' made up from random characters from 'alphabet'.
+    It's ensured that the same character cannot appear two times in a row
+    chainLength: Expects an integer > 0 to determine the length of the string output
+    alphabet: A Python list containing all valid characters
+    """
+    output = ""
+    prevChar = ""
+    for i in range(chainLength):
+        selectedChar = random.choice(alphabet)
+        while selectedChar==prevChar:
+            selectedChar = random.choice(alphabet)
+        output += selectedChar
+        prevChar = selectedChar
+    return output
 
 def generate_chains(name, nChains, minLength, maxLength, alphabet):
     output = ""
     for i in range(nChains):
+        chainLength = random.choice(range(minLength, maxLength+1))
         output += name+str(i+1)+"\n"
-        output += generate_random_sequence(minLength, maxLength, alphabet)
+        output += generate_different(chainLength, alphabet)
         output += "\n"
     return output
 
@@ -24,7 +87,7 @@ def generate_fastq(nChains, minLength, maxLength, alphabet):
     return generate_chains("@read", nChains, minLength, maxLength, alphabet)
 
 def main():
-    print(generate_fastq(10, 10, 20, string.ascii_lowercase[:26]))
+    print(generate_fastq(10, 10, 20, ['a','c','g','t']))
 
 if __name__=='__main__':
     main()
