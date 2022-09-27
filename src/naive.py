0e2668d3b27469cbdffcf4b6c0d97d229e235e0d
Győrffy Anna <annesparksie@gmail.com>
2022-09-27 18:53:08 +0200

    Simpler naive

--- src/naive.py
+++ src/naive.py
@@ -7,16 +7,11 @@ import fastq
 def naive(read, genome):
     length = len(read[1])
     for i in range(len(genome[1]) - len(read[1]) + 1):
-        current_match_length = 0
         for j in range(len(read[1])):
-            if genome[1][i + j] == read[1][j]:
-                current_match_length += 1
-            else:
-                current_match_length = 0
+            if genome[1][i + j] != read[1][j]:
                 break
-            
-            if current_match_length == length:
-                print(f"{read[0]}\t{genome[0]}\t{i + 1}\t{length}M\t{read[1]}")
+        else:
+            print(f"{read[0]}\t{genome[0]}\t{i + 1}\t{length}M\t{read[1]}")
 
         
 
